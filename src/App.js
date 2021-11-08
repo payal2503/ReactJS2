1504ab99dc309995cace5ee429d27f5771a44ec6
Payal <singhpayal2503@gmail.com>
2021-11-08 13:45:56 -0800

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './Redux/configureStore';
 
-
-
 const store = ConfigureStore();
 
 class App extends Component {
-
   
   render(){
     return (
@@ -20,8 +17,7 @@ class App extends Component {
         <Main />
         </div>
       </BrowserRouter>
-      </Provider>
-      
+      </Provider>     
     );
   }
 }
