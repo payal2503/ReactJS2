22211404047f1ed6d0aa462d1a7fd1c9cb7192ef
Payal <singhpayal2503@gmail.com>
2021-11-10 13:10:05 -0800

    React Redux Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -7,8 +7,11 @@ import { Control , LocalForm , Errors } from 'react-redux-form';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len); 
 const minLength = (len) => (val) => (val) && (val.length >= len); 
+// const required = (val) => val && val.length;
+// const maxLength = (len) => (val) => !(val) || (val.length <= len); 
+// const minLength = (len) => (val) => (val) && (val.length >= len); 
 const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(val);
+const validEmail = (val) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(val);
 // /^[a-zA-Z0-9._-]+:  Means that the email address must begin with alpha-numeric characters (both lowercase and uppercase characters are allowed). It may have periods,underscores and hyphens.
 // [a-zA-Z0-9.-]+: After the ‘@’ sign there must be some alpha-numeric characters. It can also contain period (‘.’) and and hyphens(‘-‘).
 // \.: After the second group of characters there must be a period (‘.’). This is to separate domain and subdomain names.
